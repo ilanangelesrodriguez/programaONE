07ae9c739bb24b8a4e21e615c362a5daced87a1d
Ilan Néstor Angeles Rodriguez <ilanangelesrodriguez@gmail.com>
2022-12-02 12:14:43 -0500

    Cambio de variable de salto de linea

--- LogicaDeProgramacion/calculo_consumo.js
+++ LogicaDeProgramacion/calculo_consumo.js
@@ -1,22 +1,23 @@
 document.write("<h3>¿Alcohol o gasolina?</h3>")
+//let saltoDeLinea=document.write("<br><br>")
 document.write("<hr>")
 document.write("<h4>Gasolina</h4>")
 alert("Uso de Gasolina")
 let capacity=40
 document.write("Capacidad de tanque: "+capacity+" litros.")
-document.write("<br>")
+let saltoDeLinea=document.write("<br><br>")
 let route=prompt("¿Cual es el recorrido del carro?")
 document.write("Recorrido: "+route+" km.")
-document.write("<br>")
+saltoDeLinea
 let efficiency=route/capacity
 document.write("Eficiencia "+efficiency+" km/litros")
 
 document.write("<h4>Alcohol</h4>")
 alert("Uso de alcohol")
 document.write("Capacidad de tanque: "+capacity+" litros.")
-document.write("<br>")
+saltoDeLinea
 route=prompt("¿Cual es el recorrido del carro?")
 document.write("Recorrido: "+route+" km.")
-document.write("<br>")
+saltoDeLinea
 efficiency=route/capacity
 document.write("Eficiencia "+efficiency+" km/litros")
