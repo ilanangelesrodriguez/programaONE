7c70298a07e5cecabfe507e3eb83b7e0eb100ea6
Ilan Néstor Angeles Rodriguez <ilanangelesrodriguez@gmail.com>
2022-12-02 14:06:04 -0500

    Funcion para saltar linea

--- LogicaDeProgramacion/calculo_consumo.js
+++ LogicaDeProgramacion/calculo_consumo.js
@@ -1,23 +1,28 @@
+function saltarLinea() {
+    document.write("<br><br>")
+}
+
 document.write("<h3>¿Alcohol o gasolina?</h3>")
-//let saltoDeLinea=document.write("<br><br>")
 document.write("<hr>")
 document.write("<h4>Gasolina</h4>")
 alert("Uso de Gasolina")
 let capacity=40
 document.write("Capacidad de tanque: "+capacity+" litros.")
-let saltoDeLinea=document.write("<br><br>")
+saltarLinea()
 let route=prompt("¿Cual es el recorrido del carro?")
 document.write("Recorrido: "+route+" km.")
-saltoDeLinea
+saltarLinea()
 let efficiency=route/capacity
 document.write("Eficiencia "+efficiency+" km/litros")
+saltarLinea()
 
 document.write("<h4>Alcohol</h4>")
 alert("Uso de alcohol")
 document.write("Capacidad de tanque: "+capacity+" litros.")
-saltoDeLinea
+saltarLinea()
 route=prompt("¿Cual es el recorrido del carro?")
 document.write("Recorrido: "+route+" km.")
-saltoDeLinea
+saltarLinea()
 efficiency=route/capacity
 document.write("Eficiencia "+efficiency+" km/litros")
+//document.write(saltoDeLinea)
