98999b99cf137555d8a0b642f99f45ac0ce7523e
Ilan Néstor Angeles Rodriguez <ilanangelesrodriguez@gmail.com>
2022-12-03 14:20:37 -0500

    Utilizo comentarios para describir las funciones

--- LogicaDeProgramacion/calcular_IMC.js
+++ LogicaDeProgramacion/calcular_IMC.js
@@ -6,23 +6,45 @@ function pointBreak() {
 }
 function calcularIMC(peso, altura) {
     imc=peso/(altura**2)
+    //Imprimir el IMC de la persona
     print("<b>El IMC es: </b>"+imc)
+    //Retornar el valor del IMC
+    return imc
+}
+function average(imc, nPersonas) {
+    average=imc/nPersonas
+    //Imprimir el promedio de n° personas
+    print("<b>El promedio de IMC es: </b>"+average)
 }
 
 print("<h3>Indice de Masa Corporal (IMC)</h3>")
-//N° de personas a calcular
+let acm=0;
 
+//N° de personas a calcular
 let n=prompt("Introduce N° de personas")
-//Peso de la persona
+//Bucle para preguntar n° veces
 for (let i = 0; i < n; i++) {
-    alert("Persona "+(i+1))
+    //Nombre de la persona
+    let name=prompt("Nombre de Persona "+(i+1))
+    alert(name)
+    print("<h4>Persona "+(i+1)+": "+name+"</h4>")
+
+    //Peso de la persona
     let w=prompt("Cual es el peso (en kg)?")
-    print("<h4>Persona "+(i+1)+"</h4>")
     print("<b>El peso es: </b>"+w+"kg")
     pointBreak()
+
+    //Altura de la persona
     let h=prompt("Cual es la altura (en m)?")
     print("<b>La altura es: </b>"+h+"m")
     pointBreak()
 
+    //Funcion para calcular el IMC y retornar el valor de IMC
     calcularIMC(w,h)
+    //Utilizo el acumulador acm y la variable de IMC
+    acm=acm+imc
 }
+pointBreak()
+pointBreak()
+//Funcion para clacular el promedio de IMC entre las personas
+average(acm,n)
