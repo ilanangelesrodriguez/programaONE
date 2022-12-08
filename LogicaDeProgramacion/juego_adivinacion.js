820ef98b3f8f40d3513bf3ee6be5b2a8f8d22f25
Ilan Néstor Angeles Rodriguez <ilanangelesrodriguez@gmail.com>
2022-12-08 10:23:08 -0500

    Aplico n° de intentos con un bucle while

--- LogicaDeProgramacion/juego_adivinacion.js
+++ LogicaDeProgramacion/juego_adivinacion.js
@@ -9,11 +9,32 @@ print("<h3>Juego de adivinacion</h3>")
 //Math round es una funcion que ayuda a redondear el numero
 //Math.random nos brinda un numero aleatorio entre 0 y 1
 var numPen=Math.round(Math.random()*10);
-var numLan=parseInt(prompt("Ingresa un N° entre 0-10"))
+var numLan=0
 
-if (numPen===numLan) {
-    alert("Felicitaciones, acertaste")
-    print("Acertaste")
-} else {
-    print("El numero era "+numPen)
+//N° de intentos del usuario
+var attempts=3
+//Acumulador de intentos
+let acm=0
+while (acm<attempts) {
+    //Registro de numero
+    numLan=parseInt(prompt("Ingresa un N° entre 0-10"))
+    //Condicional para respuesta
+    if (numPen===numLan) {
+        alert("Felicitaciones, acertaste en el intento "+(acm+1))
+        print("Acertaste")
+        pointBreak()
+        break;
+    } else {
+        print("El numero era "+numPen)
+        pointBreak()
+    }
+    acm++
+    alert("Te quedan "+(attempts-acm)+" intentos")
 }
+
+// if (numPen===numLan) {
+//     alert("Felicitaciones, acertaste")
+//     print("Acertaste")
+// } else {
+//     print("El numero era "+numPen)
+// }
