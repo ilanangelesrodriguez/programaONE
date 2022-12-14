cb8e3ad8b94abb9e3d432ad6dea91a2c30f6acaf
Ilan Néstor Angeles Rodriguez <ilanangelesrodriguez@gmail.com>
2022-12-14 01:03:29 -0500

    Diseño creeper con funciones de javascript

--- LogicaDeProgramacion/canvas.js
+++ LogicaDeProgramacion/canvas.js
@@ -1,4 +1,27 @@
 document.write("<h1>Hola</h1>")
 //Conectar etiquetas html con acciones en JavaScript
-let pantalla=document.querySelector("");
-let pincel=pantalla.getContext("2d")
+let pantalla=document.querySelector("canvas");
+let pincel=pantalla.getContext("2d");
+
+// pincel.fillStyle="lightgrey" //propiedad
+// pincel.fillRect(0,0,600,400) //Funcion
+
+pincel.fillStyle="green" //propiedad
+pincel.fillRect(0,0,200,400) //Funcion
+
+pincel.fillStyle="red" //propiedad
+pincel.fillRect(400,0,200,400) //Funcion
+
+pincel.fillStyle="yellow"
+pincel.beginPath();
+pincel.moveTo(300,200);
+pincel.lineTo(200,400);
+pincel.lineTo(400,400);
+pincel.fill()
+
+pincel.fillStyle="blue"
+pincel.beginPath();
+pincel.arc(300,200,50,0,2*3);
+pincel.fill()
+
+//Usar canvas rendering context 2d
---
+++ LogicaDeProgramacion/creeper.js
@@ -0,0 +1,26 @@
+let pantalla=document.querySelector("canvas");
+let pincel=pantalla.getContext("2d");
+
+//Rectangulo 1
+pincel.fillStyle="darkgreen" //propiedad
+pincel.fillRect(200,50,350,300) //Funcion
+
+//Rectangulo 2
+pincel.fillStyle="black" //propiedad
+pincel.fillRect(250,110,90,90) //Funcion
+
+//Rectangulo 3
+pincel.fillStyle="black" //propiedad
+pincel.fillRect(410,110,90,90) //Funcion
+
+//Rectangulo 4
+pincel.fillStyle="black" //propiedad
+pincel.fillRect(340,200,70,100) //Funcion
+
+//Rectangulo 5
+pincel.fillStyle="black" //propiedad
+pincel.fillRect(300,240,40,110) //Funcion
+
+//Rectangulo 6
+pincel.fillStyle="black" //propiedad
+pincel.fillRect(410,240,40,110) //Funcion
